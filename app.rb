3c53c959d944f459b103f1a6ff55c515e9ce26dc
savannaelbey <savanna_e@icloud.com>
2020-11-11 14:14:29 +0000

    creating form params

--- app.rb
+++ app.rb
@@ -14,7 +14,13 @@ get '/dogs' do
   "wooof woof!"
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
- end
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
